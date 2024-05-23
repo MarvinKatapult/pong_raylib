3a44ac4e89d1f83b3094f6f8f36f35421d5abb2b
MarvinKatapult <ibingmarvin@gmail.com>
2024-05-23 19:08:53 +0200

    Included Raylib in the project

--- build.sh
+++ build.sh
@@ -16,14 +16,31 @@ yellow_echo() {
     echo -e "\e[33m$text\e[0m"
 }
 
+########## Compiling Raylib ##########
+    echo -n "Compiling "
+    yellow_echo "Raylib"
+
+    cd raylib/src
+    make PLATFORM=PLATFORM_DESKTOP
+    error_code=$?
+    cd ../..
+
+    echo -n "Compiling "
+    if [ $error_code -eq 0 ]; then
+        green_echo "finished"
+    else 
+        red_echo "failed"
+    fi
+
 ########## Compiling Binary ##########
     echo -n "Compiling all files for project "
     yellow_echo "$project"
 ######### Add Source Files #########
-    g++ -o $project -Wextra -Wall -pedantic -g -Iinclude -lraylib \
+    g++ -o $project -Wextra -Wall -pedantic -g -Iinclude -Iraylib/src \
         src/main.cpp \
         src/paddle.cpp \
         src/ball.cpp \
+        -Lraylib/src -lraylib \
 ###################################
     error_code=$?
     echo -n "Compiling "
